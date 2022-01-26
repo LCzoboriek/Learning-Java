06ae1d474be5b1af2d2ad9c12566cdbab2458eca
Luke Czoboriek <LC169782@protonmail.com>
2022-01-26 13:41:43 +0000

    Creation of a learning application revolving around OOP within java

---
+++ src/main/java/Dog.java
@@ -0,0 +1,17 @@
+public class Dog {
+
+    private String name;
+    //generally you want to keep attributes of a class private, if you need to access that outside the class, create a method that allows that access, rather then making the attribute public
+    private String breed;
+
+    public Dog(String breedName, String name) {
+        this.breed = breedName;
+        this.name = name;
+        //'this' refers to the instance of this class
+    }
+
+    public String getName(){
+        return name;
+    }
+
+}
---
+++ src/main/java/newApplication.java
@@ -0,0 +1,10 @@
+public class newApplication {
+
+    public static void main(String[] args) {
+        Dog fido = new Dog ("collie", "fido");
+        Dog rover = new Dog("Lab", "rover");
+
+        System.out.println(fido.getName());
+        System.out.println(rover.getName());
+    }
+}
